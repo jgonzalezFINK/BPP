36dd587f6bb40aba2e6e88073ac143f8ab166fc0
ErikManuel <erik.manuel.calderon@gmail.com>
2020-10-23 17:29:48 -0500

    "nuevo usuario"

--- p-kev-api/src/App.js
+++ p-kev-api/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './components/Login'
 import Lista from './components/Lista'
 import DetallesPro from './components/DetallesPro'
+import AddUsuario from  './components/AddUsuario'
 import {BrowserRouter as Router,
   Route,Switch
 } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" exact><Login/></Route>
       <Route path="/Productos" exact><Lista/></Route>
       <Route path="/Productos/:id" exact><DetallesPro/></Route>
+      <Route path="/AddUsuario" exact><AddUsuario/></Route>
       </Switch>
     </div>
     </Router>
